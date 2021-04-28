ccaaa000baa9945eb71c8f82c367514e20c532f7
victorkang99 <victorkang99@berkeley.edu>
2021-04-28 11:45:01 +0900

    trade analyzer page activated

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return render_template("index.html", content="Testing")
+
+@app.route("/trade", methods=["POST", "GET"])
+def trade():
+	if request.method == "POST":
+		user = request.form["nm"]
+		return user
+	else:
+		return render_template("trade.html")
+
+#@app.route("/rank", methods=["POST", "GET"])
+#def rank():
+#	if requeest.method == "POST":
+#		user = request.form["nm"]
+#		return redirect(url_for("user", usr=user))
+#	else:
+#		return render_template("rank.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
